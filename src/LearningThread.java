233e00af9c4a9a89d58935bdc6658112075cf96f
Chloe <31783292+chloeqino@users.noreply.github.com>
2018-12-05 18:03:55 -0800

    Add files via upload

---
+++ src/LearningThread.java
@@ -0,0 +1,22 @@
+
+public class LearningThread extends Thread
+{
+	boolean visible;
+	AlphabetLearningFrame frame;
+	public LearningThread(boolean visible)
+	{
+		this.visible = visible;
+	}
+	public void run()
+	{
+		 frame = new  AlphabetLearningFrame();
+		 frame.setVisible(visible);
+		 
+	}
+	public void closeThis()
+	{
+		frame.closeThis();
+	}
+	
+	
+}
